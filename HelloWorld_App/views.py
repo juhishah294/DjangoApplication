043a9a9030334b16661d86fb63afa7e43fe70559
Juhi Dungrani <juhi@toystack.co>
2024-05-30 16:43:16 +0530

    Changed hello world to hello toystack to check PR on deployments

--- HelloWorld_App/views.py
+++ HelloWorld_App/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def sayHello(request):
-    return HttpResponse('<h1 style="color:red">Hello World!! Learn Django here!</h1>')
+    return HttpResponse('<h1 style="color:red">Hello Toystack!! Learn Django here!</h1>')
